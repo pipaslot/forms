a12641dc1518d5303a6ead7aa8694ca07273eb9a
Petr Štipek <p.stipek@email.cz>
2016-04-01 18:03:01 +0200

    Fieldset rendering fix for bootstrap

--- src/Forms/Rendering/Bootstrap3Renderer.php
+++ src/Forms/Rendering/Bootstrap3Renderer.php
@@ -19,11 +19,13 @@ class Bootstrap3Renderer extends AManualRenderer
 		$this->wrappers['pair']['container'] = 'div class=form-group';
 		$this->wrappers['pair']['.error'] = 'has-error';
 		$this->wrappers['control']['container'] = 'div class=col-sm-9';
-		$this->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
 		$this->wrappers['control']['description'] = 'span class=help-block';
 		$this->wrappers['control']['errorcontainer'] = 'span class=help-block';
+		$this->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
 		$this->wrappers['error']['container'] = 'div class="col-sm-9 col-sm-offset-3"';
 		$this->wrappers['error']['item'] = 'div class="alert alert-danger" role="alert"';
+		$this->wrappers['group']['container'] = 'fieldset class=form-group';
+		$this->wrappers['group']['label'] = 'legend class="col-sm-9 col-sm-offset-3"';
 	}
 
 	/**
